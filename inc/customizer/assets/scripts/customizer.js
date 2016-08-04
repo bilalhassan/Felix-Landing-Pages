d45eed56bad03252fa09889f2b4a9fe7aa867663
Eric Green <eric@smartcat.ca>
2016-08-04 09:30:21 -0400

    Cleanup code formatting

--- inc/customizer/assets/scripts/customizer.js
+++ inc/customizer/assets/scripts/customizer.js
@@ -1,19 +1,17 @@
-jQuery(document).ready( function($) {
+jQuery( document ).ready( function( $ ) {
     
-    (function ( api ) {
+    ( function ( api ) {
+        
         api.panel( 'felix_landing_page', function( panel ) {
+            
             panel.expanded.bind( function( isExpanded ) {
-                var url;
-                if ( isExpanded ) {
-                    url = FelixCustomizer.page_url;
-                }else{
-                    url = api.settings.url.home;
-                }
                 
-                api.previewer.previewUrl.set( url );
+                api.previewer.previewUrl.set( isExpanded ? FelixCustomizer.page_url : api.settings.url.home );
                 
             } );
+            
         } );
+        
     } ( wp.customize ) );
     
 });
